df6e04c5c27805c49a08ad3ad366c2df3c7c4648
hanayashiki <cwang592@usc.edu>
2021-11-14 17:39:24 +0800

    feat: add prettier

--- index.tsx
+++ index.tsx
@@ -1,19 +1,21 @@
-import express from "express";
-import { h } from "preact";
+import express from 'express';
+import { h } from 'preact';
 import { render } from 'preact-render-to-string';
-import { BaseHtml, SearchBox } from "./render";
+import { BaseHtml, SearchBox } from './render';
 
 const app = express();
 const port = 4545;
 
 app.get('/', async (req, res) => {
-  return res.send(render(
-    <BaseHtml>
-      <SearchBox />
-    </BaseHtml>
-  ))
+  return res.send(
+    render(
+      <BaseHtml>
+        <SearchBox />
+      </BaseHtml>,
+    ),
+  );
 });
 
 app.listen(port, () => {
-  console.info(`web started at http://localhost:${port}`)
+  console.info(`web started at http://localhost:${port}`);
 });
